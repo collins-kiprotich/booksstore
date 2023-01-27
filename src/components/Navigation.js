45b4f1a584fe2820cd117f03c30dae3035bd0145
collins kiprotich <collinskiprotich@gmail.com>
2023-01-27 11:23:48 +0300

    Fix linter errors

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Navigation = () => (
   <div>
-    <ul className='nav'>
+    <ul className="nav">
       <li><Link to="/">Books</Link></li>
       <li><Link to="/categories">Categories</Link></li>
     </ul>
